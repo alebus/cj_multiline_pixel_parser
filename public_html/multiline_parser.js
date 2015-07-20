2c090ed676e7cb424669770cc8fbcf285df141e2
alebus <alebus@SB-ALEBUS.corp.valueclick.com>
2015-07-20 11:38:05 -0700

    file load working

--- public_html/multiline_parser.js
+++ public_html/multiline_parser.js
@@ -1,36 +1,44 @@
-//note this needs html5
-//
-//some code from http://www.html5rocks.com/en/tutorials/file/dndfiles/
+
+
+/*
+ * 
+ * 
+ *07-20-2015
+ *note this needs HTML5 
+ *file load code was based on http://www.html5rocks.com/en/tutorials/file/dndfiles/ 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
 
 //todo also see this: https://developer.mozilla.org/en-US/docs/Using_files_from_web_applications
 
 
   function handleFileSelect(evt) {
     var files = evt.target.files; // FileList object
-
-    f = files[0];
-
     var reader = new FileReader();
 
       // Closure to capture the file information.
       reader.onload = (function(theFile) {
-        return function(e) {
+        return function() {
      
 		var theText = reader.result;
 		var myArray = theText.split(/[\n]/);
+	
+                for(i=0;i<myArray.length;i++) {
 		
-			for(i=0;i<myArray.length;i++) {
+                console.log(myArray[i]);
 		
-				console.log(myArray[i]);
+		}
 		
-			}
-		
-	 
-			
         };
-      })(f);
+      })(files[0]);
 
-   reader.readAsText(f);
+   reader.readAsText(files[0]);
 	     
   }
 
